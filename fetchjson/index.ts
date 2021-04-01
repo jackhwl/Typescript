9b033fba218891c30bbe55b243072470cba736ee
Wenlin Huang <jackhwl@hotmail.com>
2021-03-31 20:51:09 -0400

    Section 1: Getting Started with Typescript

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,6 +2,28 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  //userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(response => {
-  console.log(response.data);
+  const todo = response.data as Todo;
+
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logoTodo(id, title, completed)
 });
+
+const logoTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `);
+}
