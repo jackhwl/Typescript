b5f2c1235586fb5341738979f62c6ee0a2051d81
Wenlin Huang <jackhwl@hotmail.com>
2021-04-08 22:48:05 -0400

    linkedList sorter

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const collection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(collection);
@@ -15,3 +16,15 @@ s_sorter.sort();
 
 console.log(s_collection.data);
 
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(5);
+linkedList.add(56);
+linkedList.add(3);
+linkedList.add(-45);
+const l_sorter = new Sorter(linkedList);
+l_sorter.sort();
+
+linkedList.print();
