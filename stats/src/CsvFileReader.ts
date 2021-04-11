9fcbef06efa4390ac5f60b270b464df7251bd3e0
Wenlin Huang <jackhwl@hotmail.com>
2021-04-11 18:02:33 -0400

    Applying a Type to a Generic Class

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,13 +1,11 @@
 import fs from 'fs';
-import { dateStringToDate } from './utils';
-import { MatchResult } from './MatchResult';
 
-type MatchDate = [Date, string, string, number, number, MatchResult, string];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
-export class CsvFileReader {
-  data: MatchDate[] = [];
-  
   constructor(public filename: string) {}
+
+  abstract mapRow(row: string[]): T;  
 
   read(): void {
     this.data = fs.readFileSync(this.filename, {
@@ -15,16 +13,9 @@ export class CsvFileReader {
     })
     .split('\n')
     .map((row: string): string[] => row.split(','))
-    .map((row: string[]): MatchDate => {
-      return [
-        dateStringToDate(row[0]),
-        row[1],
-        row[2],
-        parseInt(row[3]),
-        parseInt(row[4]),
-        row[5] as MatchResult,
-        row[6]
-      ]
-    })
+    .map(this.mapRow)
   }
-}
+
+};
+
+
