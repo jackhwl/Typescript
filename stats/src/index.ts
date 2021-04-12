4f8aadad1bd9e51280434a01d92683b978371f43
Wenlin Huang <jackhwl@hotmail.com>
2021-04-12 17:35:12 -0400

    static method to avoid new instance

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,13 +1,8 @@
-import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
-import { HtmlReport } from './reportTargets/HtmlReport';
 
-const csvFileReader = new CsvFileReader('football.csv');
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
+
 matchReader.load();
-
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
 summary.buildAndPrintReport(matchReader.matches);
