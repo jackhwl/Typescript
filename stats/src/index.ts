acf016c06ff880c2fbd3c2de3985f941e25d40f8
Wenlin Huang <jackhwl@hotmail.com>
2021-04-12 10:28:20 -0400

    put it all together

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,19 +1,12 @@
 import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
 
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5]===MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5]===MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(manUnitedWins)
+const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport());
+summary.buildAndPrintReport(matchReader.matches);
