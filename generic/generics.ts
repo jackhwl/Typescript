2bb8c0f0551015e56a604f9ce7aab4c5531d8157
Wenlin Huang <jackhwl@hotmail.com>
2021-04-13 11:20:20 -0400

    function generics and generic constraints

--- generic/generics.ts
+++ generic/generics.ts
@@ -9,3 +9,34 @@ class ArrayOfAnything<T> {
 const s = new ArrayOfAnything<string>(['a', 'b', 'c']);
 
 const s2 = new ArrayOfAnything(['a', 'b', 'c']);
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<string>(['a', 'b', 'c'])
+printAnything(['a', 'b', 'c'])
+
+class Cat {
+  print() {
+    console.log('I am a cat')
+  }
+}
+
+class House {
+  print() {
+    console.log('I am a house')
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
