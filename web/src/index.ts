1e9a16a77e0ee3fe7ed6c1d5285ad97ab88122a6
Wenlin Huang <jackhwl@hotmail.com>
2021-04-13 20:42:52 -0400

    triggering Event Callbacks

--- web/src/index.ts
+++ web/src/index.ts
@@ -5,3 +5,17 @@ const user = new User({ name: 'Jack', age: 25});
 user.set({name: 'Peter'})
 console.log(user.get('name'))
 console.log(user.get('age'))
+
+user.on('change', () => {
+  console.log('change #1')
+});
+user.on('change', () => {
+  console.log('change #2')
+});
+user.on('save', () => {
+  console.log('save was triggered!')
+});
+
+user.trigger('change')
+user.trigger('save')
+user.trigger('saveasd')
