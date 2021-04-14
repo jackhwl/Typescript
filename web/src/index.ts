05cfb5ef7ccd942bdf2135cbd2a1104f246b1bb3
Wenlin Huang <jackhwl@hotmail.com>
2021-04-14 19:30:00 -0400

    Successfully Fetching Model Data

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,21 +1,7 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Jack', age: 25});
+const user = new User({ id: 1});
 
-user.set({name: 'Peter'})
-console.log(user.get('name'))
-console.log(user.get('age'))
+user.fetch();
 
-user.on('change', () => {
-  console.log('change #1')
-});
-user.on('change', () => {
-  console.log('change #2')
-});
-user.on('save', () => {
-  console.log('save was triggered!')
-});
-
-user.trigger('change')
-user.trigger('save')
-user.trigger('saveasd')
+setTimeout(() => console.log(user), 4000);
