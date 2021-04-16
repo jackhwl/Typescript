accff18462e6790ec74c17516e318a68b95f35b9
Wenlin Huang <jackhwl@hotmail.com>
2021-04-16 08:57:39 -0400

    Re-Integrating Eventing

--- web/src/index.ts
+++ web/src/index.ts
@@ -5,6 +5,7 @@ const user = new User({ id: 1});
 //user.fetch();
 
 //setTimeout(() => console.log(user), 4000);
-
-user.set({ name: 'Jack', age: 25});
-user.save();
+user.events.on('change', () => console.log('changeobject'))
+user.events.trigger('change')
+//user.set({ name: 'Jack', age: 25});
+//user.save();
