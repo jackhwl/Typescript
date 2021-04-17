b9d7c39ead24983eb966a8a70f06ffb10adc9168
Wenlin Huang <jackhwl@hotmail.com>
2021-04-17 11:11:27 -0400

    An Advanced Generic Constraint

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -15,13 +15,7 @@ export class User {
 
   constructor(private data: UserProps) {}
 
-  get(propName: string): (string | number) {
-    return this.data[propName];
-  }
 
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
 
 
 
