9ec4074c6f1752b11d783ca2fbf9a382d52b1ea7
Wenlin Huang <jackhwl@hotmail.com>
2021-04-18 17:13:19 -0400

    Model Wrapup

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,13 +1,13 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1, name: 'newer name', age: 0});
+const user = User.buildUser({ id: 1});
 
 //user.fetch();
 
 //setTimeout(() => console.log(user), 4000);
-user.on('save', () => console.log(user))
+user.on('change', () => console.log(user))
 //user.trigger('change')
 //user.set({ name: 'Jack', age: 25});
-user.save();
-//user.fetch();
+//user.save();
+user.fetch();
 //console.log(user.get('name'));
