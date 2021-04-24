13fbb8793eb13a474af8ca2c8d70db223b9d3d58
Wenlin Huang <jackhwl@hotmail.com>
2021-04-24 17:27:01 -0400

    Saving Data From a View

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -8,6 +8,7 @@ export class UserForm extends View<User, UserProps> {
       //'mouseenter:h1': this.onHeaderHover,
       'click:.set-age': this.onSetAgeClick,
       'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick,
     };
   }
 
@@ -29,6 +30,10 @@ export class UserForm extends View<User, UserProps> {
     console.log('random age set')
   }
 
+  onSaveClick = (): void => {
+    this.model.save();
+  }
+
   onHeaderHover(): void {
     console.log('h1 was mouseenter')
   }
@@ -36,13 +41,10 @@ export class UserForm extends View<User, UserProps> {
   template(): string {
     return `
       <div>
-        <h1>User Form</h1>
-        <div>User name: ${this.model.get('name')}</div>
-        <div>User age: ${this.model.get('age')}</div>
-        <input />
+        <input placeholder="${this.model.get('name')}" />
         <button class="set-name">Change Name</button>
         <button class="set-age">Set Random Age</button>
-        
+        <button class="save-model">Save</button>
       </div>
     `;
   }
