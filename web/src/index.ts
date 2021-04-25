bcfb68c924926eb435500244b763be4055c12a33
Wenlin Huang <jackhwl@hotmail.com>
2021-04-25 13:24:45 -0400

    Testing Region Mapping

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,6 @@
 import { User, UserProps } from './models/User';
 import { Collection } from './models/Collection';
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 
 const user = User.buildUser({ name: 'Jack', age: 25, id: 1});
 
@@ -24,9 +24,10 @@ const user = User.buildUser({ name: 'Jack', age: 25, id: 1});
 const root = document.getElementById('root');
 
 if (root) {
-  const userForm = new UserForm(root, user);
+  const userEdit = new UserEdit(root, user);
 
-  userForm.render();
+  userEdit.render();
+  console.log(userEdit)
 } else {
   throw new Error('Root element not found');
 }
