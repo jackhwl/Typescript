af6c3c6fce5bd25b2876c30a25c27cc6a38c01da
Wenlin Huang <jackhwl@hotmail.com>
2021-06-22 10:38:17 -0400

    233. More on Decorators

--- generic/decorators.ts
+++ generic/decorators.ts
@@ -1,16 +1,28 @@
+@classDecorator
 class Boat {
     @testDecorator
     color: string = 'red';
 
+    @testDecorator
     get formattedColor(): string {
         return `This boats color is ${this.color}`;
     }
 
     @logError('Oops, boat was sunk')
-    pilot(): void {
+    pilot(@parameterDecorator speed: string, @parameterDecorator generateWake: boolean): void {
         throw new Error();
         console.log('swish')
     }
+}
+
+function classDecorator(constructor: typeof Boat) {
+    console.log(constructor)
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+    console.log(target)
+    console.log(key)
+    console.log(index)
 }
 
 function testDecorator(target: any, key: string) {
