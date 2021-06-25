60ff091b6766e92732e18c0762d8617fdc40c810
Wenlin Huang <jackhwl@hotmail.com>
2021-06-25 14:29:43 -0400

    244. A Few Fixups

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import { router } from './routes/loginRoutes';
-import { router as controllerRouter } from './controllers/decorators/controller';
+import { AppRouter } from './AppRouter';
 //import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['asd']}));
 app.use(router);
-app.use(controllerRouter);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
