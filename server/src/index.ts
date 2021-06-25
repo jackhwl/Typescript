8c098d9d2fc1362740d3052c5926bb9f1238d22c
Wenlin Huang <jackhwl@hotmail.com>
2021-06-24 21:45:28 -0400

    243. Proof of Concept

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,17 @@
 import express, { Request, Response } from 'express';
 import { router } from './routes/loginRoutes';
+import { router as controllerRouter } from './controllers/decorators/controller';
 //import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+
+import './controllers/LoginController';
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['asd']}));
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
